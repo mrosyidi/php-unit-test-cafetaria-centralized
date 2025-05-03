c969685ccf7bcc4bcf9c7455d87c090d9905dfcc
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-03 13:40:39 +0700

    add FoodRepository.php

---
+++ app/Repository/FoodRepository.php
@@ -0,0 +1,12 @@
+<?php 
+
+    namespace Cafetaria\Repository;
+
+    use Cafetaria\Entity\Food;
+
+    interface FoodRepository
+    {
+        public function findAll(): array;
+        public function save(Food $food): void;
+        public function remove(string $name): bool;
+    }
