429a242e6633a3e76c78a85f582ba0ac0f624592
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-04 16:43:30 +0700

    update App.php use FoodRepositoryImpl.php

--- app/App.php
+++ app/App.php
@@ -3,5 +3,10 @@
     require_once __DIR__ . "/../vendor/autoload.php";
 
     use Cafetaria\Config\Database;
+    use Cafetaria\Repository\FoodRepositoryImpl;
+
+    $connection = Database::getConnection();
+
+    $foodRepository = new FoodRepositoryImpl($connection);
     
     echo "Cafetaria App" . PHP_EOL;
