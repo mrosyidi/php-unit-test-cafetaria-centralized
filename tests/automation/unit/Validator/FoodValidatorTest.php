3ee40e794de77cc5f5edbcaf6b19b5eb15e86dd0
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-07 10:01:00 +0700

    update FoodValidatorTest.php add isDuplicateReturnsTrueWhenDuplicateExists

--- tests/automation/unit/Validator/FoodValidatorTest.php
+++ tests/automation/unit/Validator/FoodValidatorTest.php
@@ -3,6 +3,7 @@
     namespace Cafetaria;
 
     use PHPUnit\Framework\TestCase;
+    use Cafetaria\Entity\Food;
     use Cafetaria\Validator\FoodValidator;
 
     class FoodValidatorTest extends TestCase
@@ -13,5 +14,16 @@
             $this->assertFalse(FoodValidator::isValidName("   "));
             $this->assertFalse(FoodValidator::isValidName(""));
             $this->assertTrue(FoodValidator::isValidName("   Soto Ayam  "));
+        }
+
+        public function testIsDuplicateReturnsTrueWhenDuplicateExists()
+        {
+            $food = $this->createMock(Food::class);
+            
+            $food->method('getName')->willReturn('Nasi Goreng');
+
+            $foods[] = $food;
+
+            $this->assertTrue(FoodValidator::isDuplicate($foods, 'nasi goreng'));
         }
     }
