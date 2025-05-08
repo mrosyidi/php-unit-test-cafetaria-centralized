2d9636ef7f0beba04bd2441ab6715dcb625cb64f
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-08 17:21:53 +0700

    update App.php menampilkan menu makanan

--- app/App.php
+++ app/App.php
@@ -6,6 +6,7 @@
     use Cafetaria\Repository\FoodRepositoryImpl;
     use Cafetaria\Service\FoodServiceImpl;
     use Cafetaria\View\FoodView;
+    use Cafetaria\Helper\InputHelper;
 
     $connection = Database::getConnection();
 
@@ -14,3 +15,41 @@
     $foodView = new FoodView($foodService);
     
     echo "Cafetaria App" . PHP_EOL;
+
+    while(true)
+    {
+        echo "MENU UTAMA" . PHP_EOL;
+        echo "1. Daftar Makanan" . PHP_EOL;
+        echo "2. Daftar Minuman" . PHP_EOL;
+        echo "3. Pemesanan" . PHP_EOL;
+        echo "4. Pembayaran" . PHP_EOL;
+        echo "5. Detail" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = InputHelper::input("Pilih");
+
+        if($pilihan == "1")
+        {
+            $foodView->showFood();
+        }else if($pilihan == "2")
+        {
+
+        }else if($pilihan == "3")
+        {
+
+        }else if($pilihan == "4")
+        {
+            
+        }else if($pilihan == "5")
+        {
+            
+        }else if($pilihan == "x")
+        {
+            break;
+        }else 
+        {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
