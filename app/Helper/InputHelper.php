4a585d115156fc24608693baed24dbfd31458044
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-11 11:17:18 +0700

    add InputHelper.php

---
+++ app/Helper/InputHelper.php
@@ -0,0 +1,31 @@
+<?php 
+
+    namespace Cafetaria\Helper;
+
+    class InputHelper
+    {
+        public static $inputStream = STDIN;
+
+        public static function input(string $info): string
+        {
+            if(trim($info) === '') 
+            {
+                throw new \InvalidArgumentException("Info tidak boleh kosong.");
+            }
+
+            if (!is_resource(self::$inputStream)) {
+                throw new \RuntimeException("Stream tidak valid.");
+            }    
+
+            echo "$info: ";
+
+            $result = fgets(self::$inputStream);
+
+            if($result === false)
+            {
+                throw new \RuntimeException("Gagal membaca masukan.");
+            }
+
+            return trim($result);
+        }
+    }
