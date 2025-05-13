08350d0238be8d180590b341cd029e5de04132a8
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-13 12:59:53 +0700

    add DrinkViewManualTest.php testViewShowDrink

---
+++ tests/manual/integration/DrinkViewManualTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+    require_once __DIR__ . "/../../../vendor/autoload.php";
+
+    use Cafetaria\Config\Database;
+    use Cafetaria\Repository\DrinkRepositoryImpl;
+    use Cafetaria\Service\DrinkServiceImpl;
+    use Cafetaria\View\DrinkView;
+
+
+    function testViewShowDrink()
+    {
+        $connection = Database::getConnection();
+
+        $drinkRepository = new DrinkRepositoryImpl($connection);
+        $drinkService = new DrinkServiceImpl($drinkRepository);
+        $drinkView = new DrinkView($drinkService);
+        $drinkView->showDrink();
+    }
+
+    testViewShowDrink();
