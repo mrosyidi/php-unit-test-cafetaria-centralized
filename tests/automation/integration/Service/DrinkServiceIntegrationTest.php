ef05d27e806395d758cedb065f4b84ccffc714ca
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-16 01:58:05 +0700

    add DrinkServiceIntegrationTest.php add testGetAllDrinkWithData

---
+++ tests/automation/integration/Service/DrinkServiceIntegrationTest.php
@@ -0,0 +1,36 @@
+<?php 
+
+    namespace Cafetaria;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Config\Database;
+    use Cafetaria\Entity\Drink;
+    use Cafetaria\Repository\DrinkRepositoryImpl;
+    use Cafetaria\Service\DrinkServiceImpl;
+
+    class DrinkServiceIntegrationTest extends TestCase 
+    {
+        private $drinkRepository;
+        private $drinkService;
+
+        public function setUp(): void 
+        {
+            $connection = Database::getConnection();
+            $this->drinkRepository = new DrinkRepositoryImpl($connection);
+            $this->drinkService = new DrinkServiceImpl($this->drinkRepository);
+            $this->drinkRepository->removeAll();
+        }
+
+        public function testGetAllDrinkWithData()
+        {
+            $drink = new Drink("Mie Ayam", 7000);
+            
+            $this->drinkRepository->save($drink);
+
+            $drinks = $this->drinkService->getAllDrink();
+
+            $this->assertCount(1, $drinks);
+            $this->assertEquals($drink->getName(), $drinks[0]->getName());
+            $this->assertEquals($drink->getPrice(), $drinks[0]->getPrice());
+        }
+    }
