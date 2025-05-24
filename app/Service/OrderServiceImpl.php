0b6e45ea104b511c6d33d95bde5b263198b8be25
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-24 13:56:43 +0700

    add OrderServiceImpl.php

---
+++ app/Service/OrderServiceImpl.php
@@ -0,0 +1,32 @@
+<?php 
+
+    namespace Cafetaria\Service;
+
+    use Cafetaria\Entity\Order;
+    use Cafetaria\Repository\OrderRepository;
+    use Cafetaria\Service\OrderService;
+
+    class OrderServiceImpl implements OrderService
+    {
+        private OrderRepository $orderRepository;
+
+        public function __construct(OrderRepository $orderRepository)
+        {
+            $this->orderRepository = $orderRepository;
+        }
+
+        public function getAllOrder(): array
+        {
+            return $this->orderRepository->findAll();
+        }
+
+        public function addOrder(int $code, string $name, int $price, int $qty): void 
+        {
+
+        }
+
+        public function removeOrder(int $code): void
+        {
+
+        }
+    }
