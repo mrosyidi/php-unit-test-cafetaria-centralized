c7109125a05911f92164e0ac550bc8f94fedecb4
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-26 17:45:36 +0700

    add OrderValidatorTest.php add testNumberWithinRangeShouldReturnTrue

---
+++ tests/automation/unit/Validator/OrderValidatorTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+    namespace Cafetaria;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Entity\Food;
+    use Cafetaria\Validator\OrderValidator;
+
+    class OrderValidatorTest extends TestCase
+    {
+        public function testNumberWithinRangeShouldReturnTrue()
+        {
+            $items = [1, 2, 3];
+            $this->assertTrue(OrderValidator::isWithinRange($items, 2));
+        }
+    }
