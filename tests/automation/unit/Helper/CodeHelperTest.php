8f717842d0ad5819c8aeea158fc3f36bf937a2ed
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-05-26 15:34:24 +0700

    update CodeHelperTest.php add testCodeEmptyOrdersWithPayments

--- tests/automation/unit/Helper/CodeHelperTest.php
+++ tests/automation/unit/Helper/CodeHelperTest.php
@@ -3,8 +3,8 @@
     namespace Cafetaria\Helper;
 
     use PHPUnit\Framework\TestCase;
-    use Cafetaria\Helper\Entity\Order;
-    use Cafetaria\Helper\Entity\Payment;
+    use Cafetaria\Entity\Order;
+    use Cafetaria\Entity\Payment;
 
     class CodeHelperTest extends TestCase
     {
@@ -14,5 +14,17 @@
             $payments = [];
 
             $this->assertEquals(1, CodeHelper::code($orders, $payments, true));
+        }
+
+        public function testCodeEmptyOrdersWithPayments()
+        {
+            $orders = [];
+
+            $payments = [
+                new Payment(5), 
+                new Payment(10)
+            ];
+
+            $this->assertEquals(11, CodeHelper::code($orders, $payments, true));
         }
     }
