2c5c0478c4369c949dc438caf6cf10f3c78ecaba
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-03 04:40:47 +0700

    add PaymentTest.php add testConstructorWithValidValue

---
+++ tests/automation/unit/Entity/PaymentTest.php
@@ -0,0 +1,19 @@
+<?php 
+
+    namespace Cafetaria;
+    
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Entity\Payment;
+    use Cafetaria\Exception\InvalidPaymentException;
+
+    class PaymentTest extends TestCase
+    {
+        public function testConstructorWithValidValues()
+        {
+            $payment = new Payment(1, 48000, 100000);
+            $this->assertEquals(1, $payment->getCode());
+            $this->assertEquals(48000, $payment->getTotal());
+            $this->assertEquals(100000, $payment->getPay());
+            $this->assertEquals(52000, $payment->getChange());
+        }
+    }
