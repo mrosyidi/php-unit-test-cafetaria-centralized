67759fd0c200b4a06d79749464f52438e70120d5
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-04 12:03:46 +0700

    add PaymentRepositoryTest.php add testFindAllWithData

---
+++ tests/automation/unit/Repository/PaymentRepositoryTest.php
@@ -0,0 +1,41 @@
+<?php 
+
+    namespace Cafetaria;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Entity\Payment;
+    use Cafetaria\Repository\PaymentRepositoryImpl;
+    use Cafetaria\Exception\InvalidPaymentException;
+
+
+    class PaymentRepositoryTest extends TestCase
+    {
+        private $pdo;
+        private $statement;
+        private $paymentRepository;
+
+        protected function setUp(): void
+        {
+            $this->pdo = $this->createMock(\PDO::class);
+            $this->statement = $this->createMock(\PDOStatement::class);
+            $this->pdo->method('prepare')->willReturn($this->statement);
+            $this->paymentRepository = new PaymentRepositoryImpl($this->pdo);
+        }
+
+        public function testFindAllWithData()
+        {
+            $this->statement->method('execute')->willReturn(true);
+            $this->statement->method('fetchAll')->willReturn([
+                ['code' => 1, 'total' => 48000, 'pay' => 100000]
+            ]);
+
+            $payments = $this->paymentRepository->findAll();
+
+            $this->assertCount(1, $payments);
+            $this->assertInstanceOf(Payment::class, $payments[0]);
+            $this->assertEquals(1, $payments[0]->getCode());
+            $this->assertEquals(48000, $payments[0]->getTotal());
+            $this->assertEquals(100000, $payments[0]->getPay());
+            $this->assertEquals(52000, $payments[0]->getChange());
+        }
+    }
