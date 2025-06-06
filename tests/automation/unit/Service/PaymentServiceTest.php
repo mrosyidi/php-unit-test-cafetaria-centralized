a4a45e5bc33412a38b184193ccdee165f2b97fd1
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-06 15:32:01 +0700

    add PaymentServiceTest.php add testGetAllPaymentWhenDataExist

---
+++ tests/automation/unit/Service/PaymentServiceTest.php
@@ -0,0 +1,49 @@
+<?php 
+
+    namespace Cafetaria\Service;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Entity\Payment;
+    use Cafetaria\Repository\PaymentRepository;
+    use Cafetaria\Service\PaymentServiceImpl;
+    use Cafetaria\Exception\InvalidPaymentException;
+
+    class PaymentServiceTest extends TestCase 
+    {
+        private $paymentRepository;
+        private $paymentService;
+
+        public function setUp(): void 
+        {
+            $this->paymentRepository = $this->createMock(PaymentRepository::class);
+            $this->paymentService = new PaymentServiceImpl($this->paymentRepository);
+        }
+
+        public function testGetAllPaymentWhenDataExist()
+        {
+            $payments = [
+                $this->createConfiguredMock(Payment::class, [
+                    'getCode' => 1,
+                    'getTotal' => 68000,
+                    'getPay' => 100000,
+                    'getChange' => 32000
+                ]),
+                $this->createConfiguredMock(Payment::class, [
+                    'getCode' => 2,
+                    'getTotal' => 52000,
+                    'getPay' => 100000,
+                    'getChange' => 48000
+                ])
+            ];
+
+            $this->paymentRepository->method('findAll')->willReturn($payments);
+
+            $payments = $this->paymentService->getAllPayment();
+
+            $this->assertCount(2, $payments);
+            $this->assertEquals(1, $payments[0]->getCode());
+            $this->assertEquals(68000, $payments[0]->getTotal());
+            $this->assertEquals(100000, $payments[0]->getPay());
+            $this->assertEquals(32000, $payments[0]->getChange());
+        }
+    }
