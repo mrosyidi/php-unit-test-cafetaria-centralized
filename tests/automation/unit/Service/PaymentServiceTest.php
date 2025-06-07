44c142e7187e4db0bfbcf241397812734580a17e
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-07 15:10:09 +0700

    update PaymentServiceTest.php add testAddPaymentWithPayGreaterThanTotalThrowsException

--- tests/automation/unit/Service/PaymentServiceTest.php
+++ tests/automation/unit/Service/PaymentServiceTest.php
@@ -75,4 +75,10 @@
             $this->expectexception(InvalidPaymentException::class);
             $this->paymentService->addPayment(1, 72000, -100000, 28000);
         }
+
+        public function testAddPaymentWithPayGreaterThanTotalThrowsException()
+        {
+            $this->expectexception(InvalidPaymentException::class);
+            $this->paymentService->addPayment(1, 72000, 50000, 28000);
+        }
     }
