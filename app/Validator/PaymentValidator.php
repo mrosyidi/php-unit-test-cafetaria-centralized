ef89e4618c125ca69003b6f6fe56f21526c1eddf
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-08 20:04:46 +0700

    add PaymentValidator.php

---
+++ app/Validator/PaymentValidator.php
@@ -0,0 +1,42 @@
+<?php 
+
+    namespace Cafetaria\Validator;
+
+    class PaymentValidator 
+    {
+        public static function isCodeInItems(array $items, int $code): bool 
+        {
+            if(empty($items)) 
+            {
+                throw new \InvalidArgumentException('Item tidak boleh kosong.');
+            }
+    
+            $found = false;
+            $index = 0;
+
+            while($index < count($items) && !$found)
+            {
+                $item = $items[$index];
+
+                if(!is_object($item)) 
+                {
+                    throw new \InvalidArgumentException("Item pada index {$index} bukan objek.");
+                }
+
+                if(!method_exists($item, 'getCode'))
+                {
+                    throw new \InvalidArgumentException("Item pada index {$index} harus memiliki metode getCode().");
+                }
+
+                if($item->getCode() == $code) 
+                {
+                    $found = true;
+                }else 
+                {
+                    $index = $index + 1;
+                }
+            }
+
+            return $found;
+        }
+    }
