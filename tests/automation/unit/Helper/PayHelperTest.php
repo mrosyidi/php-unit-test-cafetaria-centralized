ea023334638fe92eddc7695aa4db3a52753baf97
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-09 12:19:12 +0700

    add PaymentHelperTest.php add testPayReturnCorrectTotal

---
+++ tests/automation/unit/Helper/PayHelperTest.php
@@ -0,0 +1,22 @@
+<?php 
+
+    namespace Cafetaria\Helper;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Entity\Order;
+    use Cafetaria\Helper\PayHelper;
+
+    class PayHelperTest extends TestCase
+    {
+        public function testPayReturnsCorrectTotal()
+        {
+            $orders = [
+                new Order(1, "Rawon", 12000, 2),
+                new Order(1, "Es Oyen", 15000, 2),
+                new Order(2, "Es Campur", 10000, 2)
+            ];
+
+            $total = PayHelper::pay($orders, 1);
+            $this->assertEquals(54000, $total);
+        }
+    }
