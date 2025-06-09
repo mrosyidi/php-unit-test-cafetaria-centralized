950e648db4686551e3f139534cd485e3e0be519c
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-09 12:39:26 +0700

    add PaymentHelpertest.php add testPayReturnsZeroWhenNoMatchingCode

--- tests/automation/unit/Helper/PayHelperTest.php
+++ tests/automation/unit/Helper/PayHelperTest.php
@@ -19,4 +19,16 @@
             $total = PayHelper::pay($orders, 1);
             $this->assertEquals(54000, $total);
         }
+
+        public function testPayReturnsZeroWhenNoMatchingCode()
+        {
+            $orders = [
+                new Order(1, "Rawon", 12000, 2),
+                new Order(1, "Es Oyen", 15000, 2),
+                new Order(2, "Es Campur", 10000, 2)
+            ];
+
+            $total = PayHelper::pay($orders, 4);
+            $this->assertEquals(0, $total);
+        }
     }
