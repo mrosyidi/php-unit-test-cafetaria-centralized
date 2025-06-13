7b5c7fed90c4db9ff88a3f7ef5a202736a4fe1bc
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-13 09:49:49 +0700

    add PaymentServiceIntegrationTest.php add testGetAllPaymentWithData

---
+++ tests/automation/integration/Service/PaymentServiceIntegrationTest.php
@@ -0,0 +1,39 @@
+<?php 
+
+    namespace Cafetaria;
+
+    use PHPUnit\Framework\TestCase;
+    use Cafetaria\Config\Database;
+    use Cafetaria\Entity\Payment;
+    use Cafetaria\Repository\PaymentRepositoryImpl;
+    use Cafetaria\Service\PaymentServiceImpl;
+    use Cafetaria\Exception\InvalidPaymentException;
+
+    class PaymentServiceIntegrationTest extends TestCase 
+    {
+        private $paymentRepository;
+        private $paymentService;
+
+        public function setUp(): void 
+        {
+            $connection = Database::getConnection();
+            $this->paymentRepository = new PaymentRepositoryImpl($connection);
+            $this->paymentService = new PaymentServiceImpl($this->paymentRepository);
+            $this->paymentRepository->removeAll();
+        }
+
+        public function testGetAllPaymentWithData()
+        {
+            $payment = new Payment(1, 26000, 50000, 24000);
+            
+            $this->paymentRepository->save($payment);
+
+            $payments = $this->paymentService->getAllpayment();
+
+            $this->assertCount(1, $payments);
+            $this->assertEquals($payment->getCode(), $payments[0]->getCode());
+            $this->assertEquals($payment->getTotal(), $payments[0]->getTotal());
+            $this->assertEquals($payment->getPay(), $payments[0]->getPay());
+            $this->assertEquals($payment->getChange(), $payments[0]->getChange());
+        }
+    }
