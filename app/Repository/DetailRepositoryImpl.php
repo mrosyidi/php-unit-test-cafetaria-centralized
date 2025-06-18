c18b8d3549b15c966212e7bf249b9d051ff9bbdf
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-18 19:20:46 +0700

    add DetailRepositoryImpl.php

---
+++ app/Repository/DetailRepositoryImpl.php
@@ -0,0 +1,41 @@
+<?php 
+
+    namespace Cafetaria\Repository;
+
+    use Cafetaria\Entity\Detail;
+
+    class DetailRepositoryImpl implements DetailRepository
+    {
+        private \PDO $connection;
+
+        public function __construct(\PDO $connection)
+        {
+            $this->connection = $connection;
+        }
+
+        public function findAll(): array
+        {
+            $sql = "SELECT code, name, price, qty FROM details";
+            $statement = $this->connection->prepare($sql);
+            $statement->execute();
+            $rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            $details = [];
+
+            foreach($rows as $row)
+            {
+                $detail = new Detail();
+                $detail->setCode($row['code']);
+                $detail->setName($row['name']);
+                $detail->setPrice($row['price']);
+                $detail->setQty($row['qty']);
+                $details[] = $detail;
+            }
+
+            return $details;
+        }
+
+        public function save(Detail $detail): void
+        {
+            
+        }
+    }
