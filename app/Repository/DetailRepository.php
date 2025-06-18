87568c8d888f189126c19aa7f8ffedd7df916710
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-18 19:20:12 +0700

    add DetailRepository.php

---
+++ app/Repository/DetailRepository.php
@@ -0,0 +1,11 @@
+<?php 
+
+    namespace Cafetaria\Repository;
+
+    use Cafetaria\Entity\Detail;
+
+    interface DetailRepository
+    {
+        public function findAll(): array;
+        public function save(Detail $detail): void;
+    }
