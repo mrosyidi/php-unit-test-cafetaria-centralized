28f793f0ff6f53641c4eacbe35b8410ed7a6c1e9
mrosyidi <rosyidiyuekunchen@gmail.com>
2025-06-24 13:10:25 +0700

    update PaymentViewTest.php mengubah intruksi testViewShowPayment.php

--- tests/manual/integration/PaymentViewManualTest.php
+++ tests/manual/integration/PaymentViewManualTest.php
@@ -5,8 +5,10 @@
     use Cafetaria\Config\Database;
     use Cafetaria\Repository\OrderRepositoryImpl;
     use Cafetaria\Repository\PaymentRepositoryImpl;
+    use Cafetaria\Repository\DetailRepositoryImpl;
     use Cafetaria\Service\OrderServiceImpl;
     use Cafetaria\Service\PaymentServiceImpl;
+    use Cafetaria\Service\DetailServiceImpl;
     use Cafetaria\View\PaymentView;
 
 
@@ -16,11 +18,13 @@
 
         $orderRepository = new OrderRepositoryImpl($connection);
         $paymentRepository = new PaymentRepositoryImpl($connection);
+        $detailRepository = new DetailRepositoryImpl($connection);
 
         $orderService = new OrderServiceImpl($orderRepository);
         $paymentService = new PaymentServiceImpl($paymentRepository);
+        $detailService = new DetailServiceImpl($detailRepository);
 
-        $paymentView = new PaymentView($orderService, $paymentService);
+        $paymentView = new PaymentView($orderService, $paymentService, $detailService);
         $paymentView->showPayment();
     }
 
